05508af4b59651e4415c2a2bf146995c8ab99596
Ash Weston <ash-w@live.com.au>
2022-01-22 09:33:10 +1000

    updated js

--- menu.js
+++ menu.js
@@ -1,8 +1,31 @@
-$(document).ready(function () {
-  getDrinksList();
-  function getDrinksList() {
-    var drinksListUrl =
-      "www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
-    console.log(drinksListUrl);
-  }
-});
+var APIKey = 1;
+
+function getDrinksList() {
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007")
+    .then(function (response) {
+      if (response.status !== 200) {
+        console.log(
+          "Looks like there was a problem. Status Code: " + response.status
+        );
+        return;
+      }
+
+      response.json().then(function (data) {
+        console.log(data);
+        displayCocktail(data);
+      });
+    })
+    .catch(function (err) {
+      console.log("Fetch Error :-S", err);
+    });
+}
+
+getDrinksList();
+
+function displayCocktail(cocktail) {
+  let drinkSection = document.querySelector("p.title");
+  let drinkName = document.createElement("h2");
+  drinkName.innerHTML = cocktail.drinks[0].strDrink;
+  drinkSection.appendChild(drinkName);
+  console.log(drinkName);
+}
