a98cd406fa8751dbfb80bb7a73d27efd5e42e6b5
lconnell1000 <lconnell1000@gmail.com>
2022-01-22 15:33:35 +1030

    html finished off minus background and navbar, lots to do on javascript

---
+++ assets/js/secondpage.js
@@ -0,0 +1,52 @@
+let drinkIDs = [
+    "11002",
+    "11000",
+    "11007",
+    "12362",
+    "17252",
+    "11728",
+    "11113",
+    "11003",
+    "13731 ",
+  ];
+
+  function getDrinksList() {
+      for (i=0; i<9; i++){
+    fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkIDs[0]}`
+    )
+      .then(function (response) {
+        if (response.status !== 200) {
+          console.log(
+            "Looks like there was a problem. Status Code: " + response.status
+          );
+          return;
+        }
+  
+        response.json().then(function (data) {
+          console.log(data);
+        });
+      })
+      .catch(function (err) {
+        console.log("Fetch Error :-S", err);
+      });
+  }}
+ 
+  getDrinksList();
+
+
+
+  function enterSales () {
+    $("#lanuchModalSales").click(function() {
+        $(".modal").addClass("is-active");  
+      });
+      
+      $(".modal-close").click(function() {
+         $(".modal").removeClass("is-active");
+      });
+  }
+
+  enterSales();
+  
+
+
