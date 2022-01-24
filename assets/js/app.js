2341586f467d840cc4fa5b75a931a1ced872fe77
Huy Vo <vohuy819@gmail.com>
2022-01-24 19:35:51 +1030

    add drink suggestion

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,9 @@ var setDate = moment().day(0).format('ddd');
 var currentDate = moment().format('ddd');
 var returnMenu = document.querySelector('#returnMenu');
 var returnCurrentStock=document.querySelector('#returnCurrentStock')
+var drinkType = document.querySelector('#drinkType')
+var APIonecall
+var tempArray = [];
 // fetch city's lat and lon
 function fetchCoor() {
     var link5day = 'https://api.openweathermap.org/data/2.5/forecast?q=Adelaide&cnt=6&appid='+APIkey+'&units=metric';
@@ -30,16 +33,17 @@ function fetchOnecall(lat,lon) {
     return response.json();
   })
   .then(function (data) {
-  var APIonecall = data;
+  APIonecall = data;
   futureWeather(APIonecall);
   });  
 }
-
+// display next week weather
 function futureWeather(a) {
     for (var i=1; i<7;i++){
         forecast = document.createElement('div');
         forecast.className = "tile"
-        date = document.createElement('h4')
+        date = document.createElement('h4');
+        weekDays = document.createElement('h5');
         var img = document.createElement('img');
         img.setAttribute("src", 'http://openweathermap.org/img/wn/'+a.daily[i].weather[0].icon+'@2x.png');
         futureConditions = document.createElement('ul');
@@ -47,29 +51,49 @@ function futureWeather(a) {
         futureTemp= document.createElement('li');
         nextdate=displayDate.add(1,"day");
         date.textContent= nextdate.format("DD/MM/YYYY");
+        weekDays.textContent= nextdate.format("ddd");
         futureTemp.textContent= "Temp: "+ a.daily[i].temp.day+ " \xB0C";
+        tempArray.push( a.daily[i].temp.day);
         weather.appendChild(forecast);
         forecast.appendChild(date);
+        forecast.appendChild(weekDays);
         forecast.appendChild(img);
         forecast.appendChild(futureConditions);
         futureConditions.appendChild(futureTemp);
     }
 }
-
+// launch Modal if not access on Sunday
 function launchModal() {
  modal.classList.add('is-active')
 }
-
+// redirice to current stock page
 function tocurrentstock() { 
   modal.classList.remove('is-active')
   document.location= 'https://juvenexaesthetics.com.au/wp-content/uploads/2020/05/test.png';
 }
-
+// redirect to menu page
 function toMenu() { 
   modal.classList.remove('is-active');
   document.location= 'https://juvenexaesthetics.com.au/wp-content/uploads/2020/05/test.png';
 }
 
+// suggest drink type based on weather
+function drinkSug() {
+  var sum
+  for (var i=0; i<tempArray.length;i++) {
+    sum =+ tempArray[i];
+  }
+  var avg = sum/tempArray.length;
+  if (avg>27) {
+    drinkType.textContent = 'Hot drink';
+  }
+  else {
+    drinkType.textContent = 'Cold drink'
+
+  }
+}
+
+// innit
 function innit() {
   fetchCoor();
   if (currentDate !== setDate) {
@@ -81,7 +105,9 @@ function innit() {
   closeModal.addEventListener('click',function(){
     modal.classList.remove('is-active');
   })
+  drinkSug();
 }
+
 
 innit();
 
