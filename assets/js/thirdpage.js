ac39d7866ac52686d32826a84edbd2a40e018249
Huy Vo <vohuy819@gmail.com>
2022-01-26 15:33:59 +1030

    add features for best seller and cash flow

--- assets/js/thirdpage.js
+++ assets/js/thirdpage.js
@@ -1,5 +1,6 @@
 const APIkey = 'ba9bcf4f4e1396ead860b9554bf409d3';
 var displayDate= moment();
+var bestsell = document.querySelector('#bestsell')
 var weather = document.querySelector('#weather')
 var openModal = document.querySelector('#launchModal');
 var modal = document.querySelector('.modal');
@@ -8,11 +9,14 @@ var closebtn= document.querySelector('#closebtn')
 var setDate = moment().day(0).format('ddd');
 var currentDate = moment().format('ddd');
 var returnMenu = document.querySelector('#returnMenu');
-var returnCurrentStock=document.querySelector('#returnCurrentStock')
-var drinkType = document.querySelector('#drinkType')
+var returnCurrentStock=document.querySelector('#returnCurrentStock');
+var ing = document.querySelector('#ing');
+var drinkType = document.querySelector('#drinkType');
+var sales = document.querySelector('#sales');
+var netCash = document.querySelector('#netCash');
 var APIonecall
 var tempArray = [];
-let normalStock = {
+let lastStock = {     //normal quantity for 20 serves of each
   Amaretto :30,
   Bourbon	:50,
   Campari:0,
@@ -23,6 +27,55 @@ let normalStock = {
   Tequila:	80,
   Vodka:	70,
   }
+
+const stockPrice= {
+  Amaretto :3.67,
+  Bourbon	:8.42,
+  Campari:1.73,
+  Gin:	3.4,
+  Rum: 4.74,
+  SweetVermouth: 1.98,
+  DryVermouth: 1.2,
+  Tequila:	2.92,
+  Vodka:	1.46,
+}
+
+let currentStock = {
+  Amaretto :10,
+  Bourbon	:20,
+  Campari:0,
+  Gin:	10,
+  Rum: 10,
+  SweetVermouth: 5,
+  DryVermouth: 5,
+  Tequila:	5,
+  Vodka:	5,
+}
+
+let cocktailSale = {
+  longIsland : 10,
+  mojito : 20,
+  margarita : 10,
+  tequilaFizz : 10,
+  greyhound :10,
+  manhattan : 10,
+  bloodyMarry: 10,
+  negori: 10,
+  amaretto: 10,
+}
+
+
+const drinkPrices = {
+  longIsland : 10,
+  mojito : 10,
+  margarita : 10,
+  tequilaFizz : 10,
+  greyhound :10,
+  manhattan : 10,
+  bloodyMarry: 10,
+  negori: 10,
+  amaretto: 10,
+}
 // fetch city's lat and lon
 function fetchCoor() {
     var city = 'Adelaide' //reusable if want to use for another location
@@ -64,7 +117,7 @@ function futureWeather(a) {
         nextdate=displayDate.add(1,"day");
         date.textContent= nextdate.format("DD/MM/YYYY");
         weekDays.textContent= nextdate.format("ddd");
-        futureTemp.textContent= "Temp: "+ a.daily[i].temp.day+ " \xB0C";
+        futureTemp.textContent= a.daily[i].temp.day+ " \xB0C";
         tempArray.push( a.daily[i].temp.day);
         weather.appendChild(forecast);
         forecast.appendChild(date);
@@ -73,11 +126,14 @@ function futureWeather(a) {
         forecast.appendChild(futureConditions);
         futureConditions.appendChild(futureTemp);
     }
+    NCF();
     drinkSug();
 }
 // launch Modal if not access on Sunday
 function launchModal() {
+  if (currentDate !== setDate) {
  modal.classList.add('is-active')
+  }
 }
 // redirect to current stock page
 function tocurrentstock() { 
@@ -95,9 +151,46 @@ function getStock() {
   var stock = JSON.parse(localStorage.getItem("currentStock")); 
 }
 
-// Suggest what need to buy
-function suggestNormal(){
+// Display best seller last week
+function bestSeller() {
+  let max = 'longIsland';
+  for (let i in cocktailSale) {
+    if (cocktailSale[max] < cocktailSale[i]){
+      max = `${i}`;
+    }
+  }
+  bestsell.textContent = max;
+  return max
+}
 
+
+// Calculate cash flow
+function NCF() {
+  drinksale();
+  ingCost();
+  let net =drinksale()- ingCost();
+  netCash.textContent=net;
+}
+// Calculate sale
+function drinksale() {
+  let earning =0;
+  for (let i in drinkPrices) {
+    let sold = cocktailSale[i]*drinkPrices[i];
+    earning += sold;
+  }
+  sales.textContent = '$'+earning;
+  console.log(earning)
+  return earning;
+}
+// Calculate ingredient costs
+function ingCost(){
+  let cost =0;
+ for (let i in stockPrice ) {
+  let sold = lastStock[i]-currentStock[i];
+  cost += sold*stockPrice[i];
+ }
+ ing.textContent='$'+cost;
+ return cost;
 }
 
 // suggest drink type based on weather
@@ -121,10 +214,9 @@ function drinkSug() {
 
 // innit
 function innit() {
+  bestSeller()
   fetchCoor();
-  if (currentDate !== setDate) {
-    launchModal();
-  }
+  launchModal();
   openModal.addEventListener('click',launchModal);
   returnMenu.addEventListener('click', toMenu);
   returnCurrentStock.addEventListener('click', tocurrentstock);
