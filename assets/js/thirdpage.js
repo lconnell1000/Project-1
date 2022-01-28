43aebf566c1d18a31a84b15e94f91253ad27d328
Huy Vo <vohuy819@gmail.com>
2022-01-29 00:28:40 +1030

    add download feature

--- assets/js/thirdpage.js
+++ assets/js/thirdpage.js
@@ -15,6 +15,7 @@ var drinkType = document.querySelector('#drinkType');
 var sales = document.querySelector('#sales');
 var netCash = document.querySelector('#netCash');
 var orderList = document.querySelector('#order');
+var downloadBtn = document.querySelector('#downloadBtn');
 var APIonecall
 var tempArray = [];
 let order = {};
@@ -375,6 +376,35 @@ function update () {
   })  
 }
 
+// Download order as csv file
+function download_table_as_csv(table_id, separator = ',') {
+  // Select rows from table_id
+  var rows = document.querySelectorAll('#' + table_id + ' tr');
+  // Construct csv
+  var csv = [];
+  for (var i = 0; i < rows.length; i++) {
+      var row = [], cols = rows[i].querySelectorAll('td, th');
+      for (var j = 0; j < cols.length; j++) {
+          // Clean innertext to remove multiple spaces and jumpline (break csv)
+          var data = cols[j].innerText.replace(/(\r\n|\n|\r)/gm, '').replace(/(\s\s)/gm, ' ')
+          data = data.replace(/"/g, '""');
+          // Push escaped string
+          row.push('"' + data + '"');
+      }
+      csv.push(row.join(separator));
+  }
+  var csv_string = csv.join('\n');
+  // Download it
+  var filename =  table_id + '_' + new Date().toLocaleDateString() + '.csv';
+  var link = document.createElement('a');
+  link.style.display = 'none';
+  link.setAttribute('target', '_blank');
+  link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_string));
+  link.setAttribute('download', filename);
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
 
 // innit
 function innit() {
@@ -391,6 +421,8 @@ function innit() {
     modal.classList.remove('is-active');
   })
   update()
+  downloadBtn.addEventListener('click',function(){
+    download_table_as_csv('order')});
 }
 
 
